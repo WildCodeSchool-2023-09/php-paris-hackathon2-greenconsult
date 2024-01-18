7ac6549e13741287b6bdd856fa0476d2b539d46e
Eddy <125514698+KarlRow@users.noreply.github.com>
2024-01-18 18:07:37 +0100

    update

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,14 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+
+const horaires = document.querySelectorAll('.grid-item');
+for(let i = 0 ; i < horaires.length ; i++) {
+    horaires[i].addEventListener('click', function() {
+        for(let j = 0 ; j < horaires.length ; j++) {
+            horaires[j].classList.remove('selected');
+        }
+        horaires[i].classList.toggle('selected');
+    })
+}

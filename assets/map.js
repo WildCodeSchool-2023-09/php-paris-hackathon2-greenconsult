aea17511592a6d31552c831b96d78bcf1dd106b6
2z75 <zinedine.chader@gmail.com>
2024-01-18 18:09:26 +0100

    map chakal

---
+++ assets/map.js
@@ -0,0 +1,5 @@
+// fetch('/coiffeurs?latitude=<lat>&longitude=<long>')
+//     .then(response => response.json())
+//     .then(coiffeurs => console.log(coiffeurs))
+//     .catch((err) => console.error(err));
+
